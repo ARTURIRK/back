52beff77ff2c07b949e95de57bc313fbe86889df
Артур Цебаев <arturcebaev@Artur--MacBook-Pro.local>
2025-02-15 17:44:40 +0300

    change json server config

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const fs = require("fs");
 const jsonServer = require("json-server");
-const path = require("path");
 const server = jsonServer.create();
-
-const router = jsonServer.router(path.resolve(__dirname, "db.json"));
-
-server.use(jsonServer.defaults({}));
-server.use(jsonServer.bodyParser);
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(router);
+server.listen(port);
